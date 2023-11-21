b3da8a02e7d828778a321b8ce8f355f0988289c6
Bence <bence00504@gmail.com>
2023-11-21 10:16:37 +0100

    sokmindent csinaltam, ha hiszed, ha nem szia! :)

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const gameArea= document.querySelector('#gamearea')
+const container = document.getElementById('grid-container')
+const numbers = Array.from({ length: 12 }, (_, index) => index + 1)
+let clickedNumbers = []
 
-let szamDoboz;
-for (let i = 0; i<12; i++) {
-    szamDoboz = document.createElement('div')
-    szamDoboz.innerHTML='A'
-    gameArea.appendChild(szamDoboz)
+numbers.sort(() => Math.random() - 0.5)
+
+for (let i = 0; i < 12; i++) {
+  const box = createBox(numbers[i])
+  container.appendChild(box)
 }
 
-console.log(szamDoboz)
+function createBox(number) {
+  const box = document.createElement('div')
+  box.classList.add('box')
+  box.innerHTML = number
+
+  box.addEventListener('click', function () {
+    if (parseInt(box.innerHTML) === clickedNumbers.length + 1) {
+      clickedNumbers.push(parseInt(box.innerHTML))
+      box.style.visibility = 'hidden'
+
+      if (clickedNumbers.length === numbers.length) {
+        alert('Gratulálok! Nyertél!')
+        location.reload()
+      }
+    } else {
+      alert('Rossz sorrend! Próbáld újra!')
+      location.reload()
+    }
+  })
+
+  return box
+}
